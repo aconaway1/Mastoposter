033b8fbb3763866cf82e415c91bc2f9a05defa69
Aaron Conaway <40375373+aconaway1@users.noreply.github.com>
2023-02-28 10:31:05 -0500

    * Moved toot text to CLI
      python3 main.py "This is some text to toot."
    
    * Converted to Jinja2 template
    **post_template.j2** : The template for the message. Takes msg_data from the script to render the msg.
    
    * Added Python logging functionality
    ** Logs to `output.log'

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
-# import mastodon
 from mastodon import Mastodon
 import yaml
+import sys
+from jinja2 import Template
+import logging
 
-with open("msg.txt") as file:
-    toot_text = file.read()
+TEMPLATE_FILE = "post_template.j2"
+LOG_FILE = "output.log"
+LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 
+logging.basicConfig(
+    filename=LOG_FILE,
+    format=LOG_FORMAT,
+    level=logging.DEBUG
+)
+
+msg_data = {}
+
+msg_data['msg'] = sys.argv[1]
+logging.debug(f"Got the message from CLI: {msg_data['msg']}")
+
+with open(TEMPLATE_FILE) as file:
+    template = Template(file.read())
+    
 with open("event.yml") as file:
-    event_data = yaml.safe_load(file)
+    msg_data['event'] = yaml.safe_load(file)
+    logging.debug(f"Got the event data.")
 
 with open("presenter.yml") as file:
-    presenter_data = yaml.safe_load(file)
+    msg_data['presenter'] = yaml.safe_load(file)
+    logging.debug(f"Got the presenter data.")
 
 with open("creds.yml") as file:
     creds_data = yaml.safe_load(file)
+    logging.debug(f"Got the cred data.")
+    
+msg = template.render(msg_data)
+logging.debug(f"Rendered the template: {msg}")
+    
+masto_conn = Mastodon(access_token=creds_data['token'], api_base_url=creds_data['base_url'])
+logging.debug(f"The Mastodon connection: {dict(masto_conn)}")
 
-presenter_text = f"{presenter_data['name']} is at {presenter_data['reference']}"
-
-tag_list = ""
-for tag in event_data['tags']:
-    tag_list = f"{tag_list}#{tag} "
-
-msg = f"{toot_text}\n\n{presenter_text}\n{tag_list}"
-
-print(msg)
-# masto_conn = Mastodon(access_token=creds_data['token'], api_base_url=creds_data['base_url'])
-#
 # masto_conn.toot(msg)
