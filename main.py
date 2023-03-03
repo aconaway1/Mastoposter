4b35fdc158ccd1457672130d62f6484812df55d1
Aaron Conaway <40375373+aconaway1@users.noreply.github.com>
2023-03-03 15:16:19 -0500

    Pylint. :(

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-from mastodon import Mastodon
-import yaml
+"""
+Posts a status update to Mastodon using a jinja2 template and data taken from YAML files.
+
+This script is targeted to be used during event like Tech Field Day where lots of posts are made
+so that each post is consistent and includes all the right stuff like links and tags.
+"""
 import sys
-from jinja2 import Template
 import logging
+from jinja2 import Template
+import yaml
+from mastodon import Mastodon
 
 TEMPLATE_FILE = "post_template.j2"
 LOG_FILE = "output.log"
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 
 def get_spoiler():
+    """
+    Sets the spoiler text for the post based on the `presenter.yml` file
+
+    Returns:
+        string: The spoiler text discovered
+    """
 # Get the spoiler text
     if 'spoiler' not in msg_data['presenter'].keys():
         return None
@@ -24,31 +36,30 @@ logging.basicConfig(
 msg_data = {}
 
 msg_data['msg'] = sys.argv[1]
-logging.debug(f"Got the message from CLI: {msg_data['msg']}")
+logging.debug("Got the message from CLI: %s", msg_data['msg'])
 
-with open(TEMPLATE_FILE) as file:
+with open(TEMPLATE_FILE, encoding="UTF-8") as file:
     template = Template(file.read())
-    
-with open("event.yml") as file:
+
+with open("event.yml", encoding="UTF-8") as file:
     msg_data['event'] = yaml.safe_load(file)
-    logging.debug(f"Got the event data.")
+    logging.debug("Got the event data.")
 
-with open("presenter.yml") as file:
+with open("presenter.yml", encoding="UTF-8") as file:
     msg_data['presenter'] = yaml.safe_load(file)
-    logging.debug(f"Got the presenter data.")
+    logging.debug("Got the presenter data.")
 
-with open("creds.yml") as file:
+with open("creds.yml", encoding="UTF-8") as file:
     creds_data = yaml.safe_load(file)
-    logging.debug(f"Got the cred data.")
-    
+    logging.debug("Got the cred data.")
+
 msg = template.render(msg_data)
-logging.debug(f"Rendered the template:\n---\n{msg}\n---")
+logging.debug("Rendered the template:\n---\n%s\n---", msg)
 
 
 masto_conn = Mastodon(access_token=creds_data['token'], api_base_url=creds_data['base_url'])
-logging.debug(f"The Mastodon connection: {masto_conn}")
+logging.debug("The Mastodon connection: %s", masto_conn)
 
-# toot_result = masto_conn.toot(msg)
 toot_result = masto_conn.status_post(msg, spoiler_text=get_spoiler())
 logging.info(toot_result)
-logging.debug(f"Posted message {toot_result['id']}")
+logging.debug("Posted message %s", toot_result['id'])
