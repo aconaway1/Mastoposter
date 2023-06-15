990eb030779f046e5047ba85740cc0bb20a910c4
Aaron Conaway <40375373+aconaway1@users.noreply.github.com>
2023-06-15 10:14:15 -0400

    - Added argument parsing     - Add a content warning to the toot     - Don't add the presenter info in the toot     - Don't add the event info in the toot (at this time, only hashtags)     - Ignore the template altogether and just send text - Updated template so it doesn't error out when presenter, tags, etc., aren't send to be rendered - Linted - Updated README.md

--- README.md
+++ README.md
@@ -1,47 +1,78 @@
 # TFD-Poster
 ## Summary
-This is a script to post updates to Mastodon while attending Tech Field Day events. It's very, very simple, but this will help keep a consistent format to my
-updates while an event is going on.
+This is a script to post updates to Mastodon while live-Tooting streaming events. It's very, very simple, but this will help keep a consistent format to any updates while an event is going on.
+
+For each event, you update the `event.yml` file with the proper information, and, as each presenter appears, update the `presenter.yml` file with their information. When you Toot, the message will be consistent and accurate based on the Jinja2 template. 
+
+See file section below for details on the YAML files. Pay specific attention to `creds.yml` to make sure you can post to your account.
 
 ## Usage
+### Simple
 `python main.py "YOUR MESSAGE HERE"`
 
 You can change the format of the update by modifying the `post_template.j2` file.
 
+### Optional Flags
+`-v`: Print the version
+
+`-c` "CONTENT WARNING TEXT": Add an additional content warning. If the presenter has a `cw` directive already (see below), the text of the content warning will be "ARG TEXT - YAML TEXT".
+
+`-o`: Offline mode. Do everything except actually post to Mastodon. This is good for testing a template change and makig sure you have all the Jinja2 spacing correct (which always takes me 8472848 years to fix).
+
+`-t`: Ignore the template. Post the text as-is with no formatting. The content warning from file or argument still applies.
+
+#### Data Flags
+These flags are used to *not* send various data to the template. Depending how your template looks, you may want to suppress sending certain data.
+
+`-p`: Ignore the presenter information. The content warning from file will also be ignored.
+
+`-e`: Ignore the event information. That is, don't include the hashtags.
+
 ## Files
 ### main.py
 This is the main Python script to run. It will load up the credentials, event data, presenter data, and some
-message text and send a consistently-formatted update to my Mastodon account.
+message text and send a consistently-formatted update to your Mastodon account.
 
 ### creds.yml
 A YAML file with the URL and token for Mastodon access.
 
-`token`: The token the Mastodon server gave me.
+`token`: The token the Mastodon server gave you. See the `Development` section of your instance's preferences page for info on that.
 
 `base_url` The hostname of the Mastodon server (e.g., `mastodon.social`)
 
 ### event.yml
 A YAML file with the event data.
 
-`name`: The name of the event (e.g., `Network Field Day 38383`, `Tech Field Day 9499`)
+`name`: (NOT USED) The name of the event (e.g., `Streaming Event 2023-8`, `Gaming Super-special Announcement`)
 
-`tags`: A list of tags to include in each update (e.g., `NFD38383`, `TFD9499`)
+`tags`: A list of tags to include in each update (e.g., `SE2023-8`, `GSSA`)
+
+`url`: (NOT USED) The URL of the event, usually a link to a stream
 
 ### presenter.yml
 A YAML file with information on the current presenter.
 
-`name`: The name of the presenting company (e.g., `Cisco', `Juniper`)
+`name`: The name of the presenting company (e.g., `MegaStreamer`, `Big Tech Developer`)
 
-`where`: The online presence of the presenter (e.g., `cisco@twitter.com`, `aconaway@masto.ai`) # This needs some work!
+`where`: The online presence of the presenter (e.g., `examplemegastreamer@mastodon.yourmom`)
+
+`cw`: (Optional) If you want all posts to have a content warning, enter some text here.
 
 ### post_template.j2
 This is the Jinja2 template for the message formatting. This is what you'll update to change the format of the update.
 
-The default format sends the message, the name and online presence of the presenter, and the event hashtags.
+The default format sends the message, the name and online presence of the presenter, and the event hashtags. There will be more functionality later.
 
 ```
 YOUR MESSAGE HERE
 
-Presenter A can be found at @presenterA@twitter.com
+Presenter A can be found at @presenterA@somemastodoninstance
+
 #hashtag1 #hashtag2 #hashtag3
 ```
+
+## Notes
+
+## Contact
+
+You can contact me via email at aaron@aconaway.com.
--- main.py
+++ main.py
@@ -1,65 +1,146 @@
 """
 Posts a status update to Mastodon using a jinja2 template and data taken from YAML files.
 
-This script is targeted to be used during event like Tech Field Day where lots of posts are made
-so that each post is consistent and includes all the right stuff like links and tags.
+This script is targeted to be used during streaming events where lots of posts are made.
+The idea is that each post is consistent and includes accurate tags and links as well
+as your comments.
 """
-import sys
+import argparse
 import logging
 from jinja2 import Template
 import yaml
 from mastodon import Mastodon
 
+
+VERSION = 1.1
 TEMPLATE_FILE = "post_template.j2"
 LOG_FILE = "output.log"
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 
-def get_spoiler():
+def init_args() -> argparse.ArgumentParser:
     """
-    Sets the spoiler text for the post based on the `presenter.yml` file
-
+    Initializes the CLI arguments
+    
     Returns:
-        string: The spoiler text discovered
+        argparse.ArgumentParser - The argparse parser to use later
     """
-# Get the spoiler text
-    if 'spoiler' not in msg_data['presenter'].keys():
-        return None
-    return msg_data['presenter']['spoiler']
+    parser = argparse.ArgumentParser(
+        description="Post a message to Mastodon during a streaming event",
+    )
+    parser.add_argument(
+        "-c", "--content-warning",
+        help="Add a content warning to this post",
+    )
+    parser.add_argument(
+        "-t", "--ignore-template",
+        action="store_true",
+        help="Ignore the template and post as raw message",
+    )
+    parser.add_argument(
+        "-p", "--ignore-presenter",
+        action="store_true",
+        help="Do not add the presenter info to the message",
+    )
+    parser.add_argument(
+        "-e", "--ignore-event",
+        action="store_true",
+        help="Do not add the event info to the message",
+    )
+    parser.add_argument(
+        "-o", "--offline",
+        action="store_true",
+        help="Don't actually send the message to Mastodon but do everything else."
+    )
+    parser.add_argument(
+        "-v", "--version", action="version",
+        version = f"{parser.prog} {VERSION}"
+    )
+    parser.add_argument('message')
+    return parser
 
 
-logging.basicConfig(
-    filename=LOG_FILE,
-    format=LOG_FORMAT,
-    level=logging.DEBUG
-)
-
-msg_data = {}
-
-msg_data['msg'] = sys.argv[1]
-logging.debug("Got the message from CLI: %s", msg_data['msg'])
-
-with open(TEMPLATE_FILE, encoding="UTF-8") as file:
-    template = Template(file.read())
-
-with open("event.yml", encoding="UTF-8") as file:
-    msg_data['event'] = yaml.safe_load(file)
-    logging.debug("Got the event data.")
-
-with open("presenter.yml", encoding="UTF-8") as file:
-    msg_data['presenter'] = yaml.safe_load(file)
-    logging.debug("Got the presenter data.")
-
-with open("creds.yml", encoding="UTF-8") as file:
-    creds_data = yaml.safe_load(file)
-    logging.debug("Got the cred data.")
-
-msg = template.render(msg_data)
-logging.debug("Rendered the template:\n---\n%s\n---", msg)
+def set_logging() -> None:
+    """
+    Sets up the logging location, format, and level
+    """
+    logging.basicConfig(
+        filename=LOG_FILE,
+        format=LOG_FORMAT,
+        level=logging.INFO
+    )
 
 
-masto_conn = Mastodon(access_token=creds_data['token'], api_base_url=creds_data['base_url'])
-logging.debug("The Mastodon connection: %s", masto_conn)
+def log_summary(given_cw_text: str, given_arg: argparse.Namespace, given_msg: str) -> None:
+    """
+    Logs a summary of the toot to file
+    """
+    logging.info("Summary of the toot:")
+    logging.info("CW: %s", given_cw_text)
+    logging.info("ignore_template: %s", given_arg.ignore_template)
+    logging.info("ignore_presenter %s", given_arg.ignore_presenter)
+    logging.info("ignore_event %s", given_arg.ignore_event)
+    logging.info("arg_message %s", given_arg.message)
+    logging.info("message to be sent:\n%s", given_msg)
+    print(f"{given_msg}")
 
-toot_result = masto_conn.status_post(msg, spoiler_text=get_spoiler())
-logging.info(toot_result)
-logging.debug("Posted message %s", toot_result['id'])
+def main():
+    """
+    Do stuff
+    """
+    set_logging()
+    parser = init_args()
+    arguments = parser.parse_args()
+    cw_text = ""
+    msg_data = {}
+    if arguments.content_warning:
+        logging.info("Adding a content warning to this message as \"%s\"",
+                     arguments.content_warning)
+        cw_text = arguments.content_warning
+
+    msg_data['msg'] = arguments.message
+    logging.info("Trying to post with this message:\n%s", msg_data['msg'])
+
+    if not arguments.ignore_event:
+        with open("event.yml", encoding="UTF-8") as file:
+            msg_data['event'] = yaml.safe_load(file)
+            logging.info("Got the event data.")
+    else:
+        logging.info("Ignoring the event info from file.")
+
+    if not arguments.ignore_presenter:
+        with open("presenter.yml", encoding="UTF-8") as file:
+            msg_data['presenter'] = yaml.safe_load(file)
+            logging.debug("Got the presenter data.")
+            if "cw" in msg_data['presenter']:
+                if len(cw_text) > 0:
+                    cw_text = cw_text + f" - {msg_data['presenter']['cw']}"
+                else:
+                    cw_text = msg_data['presenter']['cw']
+
+    if not arguments.ignore_template:
+        with open(TEMPLATE_FILE, encoding="UTF-8") as file:
+            template = Template(file.read())
+        rendered_msg = template.render(msg_data)
+        logging.debug("Rendered the template:\n---\n%s\n---", rendered_msg)
+    else:
+        rendered_msg = arguments.message
+
+    log_summary(given_cw_text=cw_text, given_arg=arguments, given_msg=rendered_msg)
+
+    with open("creds.yml", encoding="UTF-8") as file:
+        creds_data = yaml.safe_load(file)
+        logging.debug("Got the cred data.")
+
+    if not arguments.offline:
+        masto_conn = Mastodon(access_token=creds_data['token'], api_base_url=creds_data['base_url'])
+        logging.debug("The Mastodon connection: %s", masto_conn)
+
+        toot_result = masto_conn.status_post(rendered_msg, spoiler_text=cw_text)
+        logging.debug(toot_result)
+        logging.info("Posted message %s", toot_result['id'])
+    else:
+        logging.info("Didn't not post to Mastodon since the -o flag was given")
+
+
+if __name__ == "__main__":
+    main()
